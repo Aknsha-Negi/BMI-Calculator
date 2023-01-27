5e6f88e039c2d5693a78371e01fc9b65656fc68b
Akansha Negi <negi.akansha15@gmail.com>
2023-01-27 20:50:54 +0530

    Completed BMI Calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bmi';
+  private _height!: number;
+  private _weight!: number;
+  private _bmic!: number;
+
+  get height(){
+    return this._height;
+  }
+
+  set height(h: number){
+    this._height = h;
+  }
+
+  get weight(){
+    return this._weight;
+  }
+
+  set weight(w: number){
+    this._weight = w;
+  }
+
+  get bmic(){
+    return (this._weight*10000)/(this._height*this._height);
+  }
 }
